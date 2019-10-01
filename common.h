0a6955395090db2b69a20f09dc3916dbbdcfab76
vachan-potluri <vachanpotluri1997@gmail.com>
2019-10-01 11:51:36 +0530

    Initial commit

---
+++ common.h
@@ -0,0 +1,13 @@
+/**
+ * @file common.h
+ * @brief Contains settings common to all files
+ */
+
+#ifndef common_h
+#define common_h
+
+using uint = unsigned int;
+using namespace dealii;
+// using namespace std;
+
+#endif
