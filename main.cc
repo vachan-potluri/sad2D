d68f0bedc3d278df6547e4aaba49d314de2b9719
vachan-potluri <vachanpotluri1997@gmail.com>
2019-10-01 11:39:43 +0530

    Initial commit

---
+++ main.cc
@@ -0,0 +1,14 @@
+/**
+ * @file main.cc
+ * @brief The main file of the project
+ */
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+
+        printf("Hello World!\n");
+        return 0;
+}
