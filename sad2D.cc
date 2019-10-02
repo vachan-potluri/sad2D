7e8b5f0033e2a3ba6c27ee2839b18431bc547135
vachan-potluri <vachanpotluri1997@gmail.com>
2019-10-02 16:37:28 +0530

    Write update function

--- sad2D.cc
+++ sad2D.cc
@@ -218,6 +218,182 @@ double sad2D::calc_time_step(const double co) const
 }
 
 /**
+ * @brief Updates the solution with the given @p time_step
+ * 
+ * Algorithm:
+ * - For every cell:
+ *   - Add contribution of stiffness and damping matrices to rhs
+ *   - For every face:
+ *     - Get neighbor id
+ *     - if neighbor id > cell id, continue
+ *     - else:
+ *       - Get face id wrt owner and neighbor (using neighbor_of_neighbor)
+ *       - Get global dofs on owner and neighbor
+ *       - Using face ids and global dofs of owner and neighbor, get global dofs on this face on
+ * owner and neighbor side
+ *       - Compute the numerical flux (advective and diffusive)
+ *       - Use (advective and diffusive) lifting matrices to update owner and neighbor rhs
+ * 
+ * <code>cell->get_dof_indices()</code> will return the dof indices in the order shown in
+ * https://www.dealii.org/current/doxygen/deal.II/classFE__DGQ.html. This fact is mentioned in
+ * https://www.dealii.org/current/doxygen/deal.II/classDoFCellAccessor.html.
+ * To get the dof location, sad2D::dof_locations has been obtained using
+ * <code>DoFTools::map_dofs_to_support_points()</code>. To get normal vectors, an FEFaceValues
+ * object is created with Gauss-Lobatto quadrature of order <code>fe.degree+1</code>.
+ * 
+ * The face normal flux vector must be mapped to owner- and neighbor- local dofs for multplication
+ * with lifting matrices. The mapped vectors will be of size <code>dof_per_cell</code>.
+ * 
+ * @pre @p time_step must be a stable one, any checks on this value are not done
+ * @todo Some code repitition exists in the loop over faces
+ */
+void sad2D::update(const double time_step)
+{
+        // update old solution
+        uint i;
+        for(i=0; i<dof_handler.n_dofs(); i++) gold_solution(i) = g_solution(i);
+
+        // set rhs to zero
+        for(auto &cur_rhs: l_rhs) cur_rhs=0.0;
+
+        uint face_id, face_id_neighbor; // id of face wrt owner and neighbor
+        uint l_dof_id, l_dof_id_neighbor; // dof id (on a face) dof wrt owner and neighbor
+        // global dof ids of owner and neighbor
+        std::vector<uint> dof_ids(fe.dofs_per_cell), dof_ids_neighbor(fe.dofs_per_cell);
+        double phi, phi_neighbor; // owner and neighbor side values of phi
+        double cur_anormal_flux; // advective normal flux at current dof
+        double cur_dvalue; // (diffusive) average value on face at current dof
+        // the -ve of advective normal num flux vector of face wrt owner and neighbor
+        Vector<double> neg_anormal_flux(fe.dofs_per_cell),
+                neg_anormal_flux_neighbor(fe.dofs_per_cell);
+        // (diffusive) average values of phi on current face wrt owner and neighbor
+        Vector<double> dvalues(fe.dofs_per_cell), dvalues_neighbor(fe.dofs_per_cell);
+        Point<2> dof_loc; // dof coordinates (on a face)
+        Tensor<1,2> normal; // face normal from away from owner at current dof
+        FEFaceValues<2> fe_face_values(fe, QGaussLobatto<1>(fe.degree+1), update_normal_vectors);
+
+        for(auto &cell: dof_handler.active_cell_iterators()){
+                cell->get_dof_indices(dof_ids);
+                // compute stiffness and damping term
+                Vector<double> lold_solution(fe.dofs_per_cell); // old phi values of cell
+                for(i=0; i<fe.dofs_per_cell; i++) lold_solution[i] = gold_solution[dof_ids[i]];
+                stiff_mats[cell->index()].vmult_add(
+                        l_rhs[cell->index()],
+                        lold_solution
+                );
+                lold_solution *= -1.0; // change sign of old soln
+                damp_mats[cell->index()].vmult_add(
+                        l_rhs[cell->index()],
+                        lold_solution
+                );
+                // advective and diffusive face terms
+                for(face_id=0; face_id<GeometryInfo<2>::faces_per_cell; face_id++){
+                        if(cell->face(face_id)->at_boundary()){
+                                // this face is part of boundary, set phi_neighbor appropriately
+                                fe_face_values.reinit(cell, face_id);
+                                for(i=0; i<fe_face.dofs_per_face; i++){
+                                        l_dof_id = face_first_dof[face_id] +
+                                                i*face_dof_increment[face_id];
+                                        
+                                        normal = fe_face_values.normal_vector(i);
+                                        // owner and neighbor side dof locations will match
+                                        dof_loc = dof_locations[
+                                                dof_ids[ l_dof_id ]
+                                        ];
+
+                                        phi = gold_solution[
+                                                dof_ids[ l_dof_id ]
+                                        ];
+                                        // use array of functions (or func ptrs) to set BC
+                                        phi_neighbor =
+                                                bc_fns[cell->face(face_id)->boundary_id()](phi);
+
+                                        cur_anormal_flux = adv_diff::rusanov_aflux(phi,
+                                                phi_neighbor, normal, dof_loc);
+                                        neg_anormal_flux(l_dof_id) = -cur_anormal_flux;
+                                        cur_dvalue = 0.5*(phi+phi_neighbor);
+                                        dvalues(l_dof_id) = cur_dvalue;
+                                } // loop over face dofs
+
+                                // multiply normal flux with advective lift matrx and store add to rhs
+                                alift_mats[cell->index()][face_id].vmult_add(
+                                        l_rhs[cell->index()],
+                                        neg_anormal_flux
+                                );
+                                // multiply average value with diffusive life matrix and add to rhs
+                                dlift_mats[cell->index()][face_id].vmult_add(
+                                        l_rhs[cell->index()],
+                                        dvalues
+                                );
+                        }
+                        else if(cell->neighbor_index(face_id) > cell->index()) continue;
+                        else{
+                                // internal face
+                                fe_face_values.reinit(cell, face_id);
+                                face_id_neighbor = cell->neighbor_of_neighbor(face_id);
+                                cell->neighbor(face_id)->get_dof_indices(dof_ids_neighbor);
+                                for(i=0; i<fe_face.dofs_per_face; i++){
+                                        l_dof_id = face_first_dof[face_id] +
+                                                i*face_dof_increment[face_id];
+                                        l_dof_id_neighbor = face_first_dof[face_id_neighbor] +
+                                                i*face_dof_increment[face_id_neighbor];
+                                        
+                                        normal = fe_face_values.normal_vector(i);
+                                        // owner and neighbor side dof locations will match
+                                        dof_loc = dof_locations[
+                                                dof_ids[ l_dof_id ]
+                                        ];
+
+                                        phi = gold_solution[
+                                                dof_ids[ l_dof_id ]
+                                        ];
+                                        phi_neighbor = gold_solution[
+                                                dof_ids_neighbor[ l_dof_id_neighbor ]
+                                        ];
+
+                                        cur_anormal_flux = adv_diff::rusanov_aflux(phi,
+                                                phi_neighbor, normal, dof_loc);
+                                        cur_dvalue = 0.5*(phi+phi_neighbor);
+                                        neg_anormal_flux(l_dof_id) = -cur_anormal_flux;
+                                        neg_anormal_flux_neighbor(l_dof_id_neighbor) = cur_anormal_flux;
+                                        dvalues(l_dof_id) = cur_dvalue;
+                                        dvalues_neighbor(l_dof_id_neighbor) = cur_dvalue;
+                                } // loop over face dofs
+
+                                // multiply normal flux with advective lift matrx and add to rhs
+                                // for both owner and neighbor
+                                alift_mats[cell->neighbor_index(face_id)][face_id_neighbor].vmult_add(
+                                        l_rhs[cell->neighbor_index(face_id)],
+                                        neg_anormal_flux_neighbor
+                                );
+                                alift_mats[cell->index()][face_id].vmult_add(
+                                        l_rhs[cell->index()],
+                                        neg_anormal_flux
+                                );
+                                // multiply average values with diffusive lift matrix and add to rhs
+                                dlift_mats[cell->neighbor_index(face_id)][face_id_neighbor].vmult_add(
+                                        l_rhs[cell->neighbor_index(face_id)],
+                                        dvalues_neighbor
+                                );
+                                dlift_mats[cell->index()][face_id].vmult_add(
+                                        l_rhs[cell->index()],
+                                        dvalues
+                                );
+                        }
+                } // loop over faces
+        } // loop over cells for assembling rhs
+
+        // Now, update
+        for(auto &cell: dof_handler.active_cell_iterators()){
+                cell->get_dof_indices(dof_ids);
+                for(i=0; i<fe.dofs_per_cell; i++){
+                        g_solution[dof_ids[i]] = gold_solution[dof_ids[i]] +
+                                l_rhs[cell->index()][i] * time_step;
+                }
+        }
+}
+
+/**
  * @brief Prints stifness, damping and the 8 lifting matrices of 0-th element
  */
 void sad2D::print_matrices() const
