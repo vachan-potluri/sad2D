80eb37193fc324bf3319f29a86fc370a993d9806
vachan-potluri <vachanpotluri1997@gmail.com>
2019-10-02 14:59:23 +0530

    Add diffusivirt variable

--- adv_diff.h
+++ adv_diff.h
@@ -12,6 +12,8 @@
 
 namespace adv_diff
 {
+        const double nu = 1.0; // diffusivity
+
         Tensor<1,2> wind(const Point<2> &loc);
         Tensor<1,2> exact_aflux(const double value, const Point<2> &loc);
         double rusanov_aflux(const double o_value, const double n_value, const Tensor<1,2> &normal,
